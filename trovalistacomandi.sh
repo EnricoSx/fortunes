e4a74d468d6a5b36c928c0b2d30f6ed89f1d342a
Enrico Sartori <enricosx@gmail.com>
2017-08-06 16:15:36 +0200

    Add files via upload

---
+++ trovalistacomandi.sh
@@ -0,0 +1,1 @@
+whatis /bin/* > comandi.txt; whatis /sbin/* >> comandi.txt ; whatis /usr/bin/* >> comandi.txt; whatis /usr/sbin/* >> comandi.txt; whatis /usr/local/bin/* >> comandi.txt; whatis /usr/local/sbin/* >> comandi.txt
